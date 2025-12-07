bd38ded22ac6b6d0af7b8f76fdc5da11d6cbb1fc
Mrboombastic <pedromiguelgomesdias2109@gmail.com>
2025-12-07 12:02:23 -0300

    script.js

---
+++ public/script.js
@@ -0,0 +1,11 @@
+/**
+ * Mr.Boombastic Downloader Web
+ * JavaScript Controller
+ */
+
+// Estado da aplicação
+const AppState = {
+    currentVideo: null,
+    apiBaseUrl: '', // Será detectado automaticamente
+    selectedFormat: null,
+   
